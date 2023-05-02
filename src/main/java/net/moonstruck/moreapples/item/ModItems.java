1c969c96ec88c8c2c4b14fde08f780944948ea3c
MoonstruckLucii <heccinbigcryptid@gmail.com>
2023-05-03 00:48:10 +0200

    Added iron_apple Added Resistance Effect

---
+++ src/main/java/net/moonstruck/moreapples/item/ModItems.java
@@ -0,0 +1,30 @@
+package net.moonstruck.moreapples.item;
+
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.moonstruck.moreapples.moreapples;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, moreapples.MOD_ID);
+
+    public static final RegistryObject<Item> IRON_APPLE = ITEMS.register("iron_apple",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)
+                    .food(new FoodProperties.Builder()
+                            .nutrition(8)
+                            .saturationMod(8f)
+                            .effect(() -> new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 8 * 60 * 20, 0), 1.0f)
+                            .build())
+            ));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
