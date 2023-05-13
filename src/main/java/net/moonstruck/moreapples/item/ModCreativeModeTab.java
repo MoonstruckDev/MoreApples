740df8dd1114c99bacf5782ab8d3a0c2a0d3232b
DancerVLT69 <dancervlt69@googlemail.com>
2023-05-13 22:02:29 +0200

    changes: 'cosmetics'

--- src/main/java/net/moonstruck/moreapples/item/ModCreativeModeTab.java
+++ src/main/java/net/moonstruck/moreapples/item/ModCreativeModeTab.java
@@ -2,12 +2,11 @@ package net.moonstruck.moreapples.item;
 
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.NotNull;
 
 public class ModCreativeModeTab {
     public static final CreativeModeTab MOREAPPLES_TAB = new CreativeModeTab("MoreApples") {
         @Override
-        public ItemStack makeIcon() {
-            return new ItemStack(ModItems.AMETHYST_APPLE.get());
-        }
+        public @NotNull ItemStack makeIcon() { return new ItemStack(ModItems.AMETHYST_APPLE.get()); }
     };
 }
